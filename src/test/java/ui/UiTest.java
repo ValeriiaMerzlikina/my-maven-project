b6ddb30b5b06df66f713130743e2c8710ee3e358
valeriamerzlikina <valeriamerzlikina99@gmail.com>
2025-04-08 12:59:51 +0300

    Added gitignore

--- src/test/java/ui/UiTest.java
+++ src/test/java/ui/UiTest.java
@@ -2,18 +2,18 @@ package ui;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+
 
 public class UiTest {
     @Test
     @Tag("ui")
-    void testLoginPage() {
+   public void testLoginPage() {
         System.out.println("UI Test 1 (Thread: " + Thread.currentThread().getId() + ")");
     }
 
     @Test
     @Tag("ui")
-    void testDashboard() {
+   public void testDashboard() {
         System.out.println("UI Test 2 (Thread: " + Thread.currentThread().getId() + ")");
     }
 }
